47f29a566e43fa21917b984ba9ef3bd7ffbaed15
lunafish <lunafish@root93.com>
2013-05-15 19:33:08 +0900

    connect DB

--- request.php
+++ request.php
@@ -4,4 +4,14 @@ $name = $_REQUEST['name'];
 
 echo $name;
 echo " welcome!";
+
+$dbbase = 'projectcc';
+$dbuser = 'root';
+$dbpass = 'root93.net';
+$dbhost = 'localhost:3306';
+
+$dblink = mysql_connect($dbhost, $dbuser, $dbpass) or die("System down");
+
+mysql_select_db($dbbase, $dblink) or die("Database Down");
+
 ?>
